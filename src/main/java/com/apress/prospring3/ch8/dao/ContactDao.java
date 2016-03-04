06d30beeb962e495d3af9794c34c793182d8e4c9
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-03-05 01:30:15 +0200

    Add annotation config

--- src/main/java/com/apress/prospring3/ch8/dao/ContactDao.java
+++ src/main/java/com/apress/prospring3/ch8/dao/ContactDao.java
@@ -7,17 +7,17 @@ import com.apress.prospring3.ch8.domain.Contact;
 public interface ContactDao {
     public List<Contact> findAll();
 
+    public List<Contact> findAllWithDetail();
+
     public List<Contact> findByFirstName(String firstName);
+
+    public String findFirstNameById(Long id);
+
+    public String findLastNameById(Long id);
 
     public void insert(Contact contact);
 
     public void update(Contact contact);
 
     public void delete(Long contactId);
-
-    public String findFirstNameById(long id);
-
-    public String findLastNameById(long id);
-
-    public List<Contact> findAllWithDetail();
 }
---
+++ src/main/java/com/apress/prospring3/ch8/dao/jdbc/annotation/JdbcContactDao.java
@@ -0,0 +1,59 @@
+package com.apress.prospring3.ch8.dao.jdbc.annotation;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import com.apress.prospring3.ch8.domain.Contact;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Repository;
+import com.apress.prospring3.ch8.dao.ContactDao;
+
+import java.util.List;
+
+@Repository("contactDao")
+public class JdbcContactDao implements ContactDao {
+    private Log log = LogFactory.getLog(JdbcContactDao.class);
+    private DataSource dataSource;
+
+    @Resource(name = "dataSource")
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public List<Contact> findAll() {
+        return null;
+    }
+
+    public List<Contact> findAllWithDetail() {
+        return null;
+    }
+
+    public List<Contact> findByFirstName(String firstName) {
+        return null;
+    }
+
+    public String findFirstNameById(Long id) {
+        return null;
+    }
+
+    public String findLastNameById(Long id) {
+        return null;
+    }
+
+    public void insert(Contact contact) {
+
+    }
+
+    public void update(Contact contact) {
+
+    }
+
+    public void delete(Long contactId) {
+
+    }
+}
